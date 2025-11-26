1629af2fa4a8b78e9e1af747650182b41f81a5bc
Lauryn <lauryndube1@gmail.com>
2025-11-26 22:04:29 +0100

    add read pictures and load to Bitmap

--- app/src/main/java/com/example/picturesafe/MainActivity.java
+++ app/src/main/java/com/example/picturesafe/MainActivity.java
@@ -8,11 +8,17 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE = 1;
     ImageView imageView;
+    TextView infoText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnSelect = findViewById(R.id.btnSelect);
         imageView = findViewById(R.id.imageView);
+        infoText = findViewById(R.id.infoText);
 
         btnSelect.setOnClickListener(v -> {
             Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
@@ -33,9 +40,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+
         if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null) {
             Uri imageUri = data.getData();
-            imageView.setImageURI(imageUri);
+            try {
+                // Bild als Bitmap laden
+                InputStream inputStream = getContentResolver().openInputStream(imageUri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                inputStream.close();
+
+
+                // Anzeige
+                imageView.setImageBitmap(bitmap);
+
+                // --- Auflösung ---
+                int width = bitmap.getWidth();
+                int height = bitmap.getHeight();
+
+
+                int k = 1;
+                int s = 32;
+                int capacity = ((width * (height-1)*3*k) - (s+16)*(height-1)) / 8000;
+
+                infoText.setText(
+                        "Auflösung: " + width + " x " + height +
+                                "\nSpeicherbare Datenmenge: " + capacity + " KiloBytes"
+                );
+
+
+                // Hier hast du das Bitmap-Objekt für Steganographie
+                // z.B.: this.loadedBitmap = bitmap;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
